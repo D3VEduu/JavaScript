8338d8c0605f4847e0867137db0513cf0fda5beb
D3VEduu <edu.ardo0123@hotmail.com>
2019-08-06 20:39:32 -0300

    Feita atividade 7

--- index.php
+++ index.php
@@ -112,4 +112,90 @@ function atividade6(){
     echo "<hr>";
 }
 atividade6();
+// Atividade 7
+// Efetue um algoritmo PHP que contém as variáveis Nome Funcionário, Valor Salário por Hora, Número de Horas Trabalhadas e imprima o salário reajustado com base na seguinte regra:
+// - salário até 300, reajuste de 50%
+// - salário maiores que 300, reajuste de 30%
+function atividade7(){
+    $nome = "Edu";
+    $valorhora = 40;
+    $horatrabalhadas = 45;
+    $salario = ($valorhora * $horatrabalhadas);
+
+    if($salario <= 300){
+        $salariofinal = $salario + ($salario * 0.3);
+    }
+    else {
+        $salariofinal = $salario + ($salario * 0.5);
+    }
+}
+atividade7();
+
+// Atividade 8
+// Faça um algoritmo PHP que leia um número entre 0 e 10 contido em uma variável, e escreva este número por extenso.
+function atividade8(){
+
+}
+    atividade8();
+
+// Atividade 9
+// Faça um algoritmo PHP que contém uma variável com um valor inteiro. O programa deverá então imprimir todos os valores pares, e posteriormente todos os valores ímpares. Exemplo para variável 5, imprimirá “2 4 1 3 5”.
+function atividade9(){
+
+}
+    atividade9();
+
+// Atividade 10
+// Faça um algoritmo PHP, que irá exibir a data atual por extenso do seguinte modo: “Tatuí, 31 de Julho de 2019”.
+function atividade10(){
+
+}
+    atividade10();
+
+// Atividade 11
+// Efetue um algoritmo PHP, que irá exibir uma mensagem de qual bimestre do ano estamos. Exemplo: “Estamos no 1º Bimestre de 2019”.
+function atividade11(){
+
+}
+    atividade11();
+
+// Atividade 12
+// Efetue um algoritmo PHP, que com base na sua data de nascimento exiba quantos dias de vida você possui.
+function atividade12(){
+
+}
+    atividade12();
+
+// Atividade 13
+// Efetue um algoritmo PHP que contém as variáveis Código Cargo, Nome do Trabalhador, Data de Nascimento, Tempo Contribuição. Com base nas informações abaixo, imprima a mensagem se a pessoa já está habilitada a se aposentar ou não.
+// Imprima a saída final do seguinte modo:
+// Nome: Inocêncio Coitadinho Cargo: Trabalhador Rural Tempo Contribuição: 15 anos Idade do Trabalhador: 60 anos
+// Código Cargo
+// Cargo
+// Tempo Contribuição
+// Idade Mínima
+// 1
+// Trabalhador Rural
+// 15 anos
+// 60 (homem), 55 (mulher)
+// 2
+// Trabalhador Geral
+// 20 anos
+// 65 (homem), 62 (mulher)
+// 3
+// Político
+// 20 anos
+// 65 (homem), 62 (mulher)
+// 4
+// Servidor Público
+// 25 anos
+// 65 (homem), 62 (mulher)
+// 5
+// Policial
+// 30 (homem), 25 (mulher)
+// 55 (ambos)
+function atividade13(){
+
+}
+    atividade12();
 ?>
